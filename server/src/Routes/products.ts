09ad53467e9ae8fb274e3cdfff9096c280848f2d
madahva <galarzaguillermo.ggm@gmail.com>
2023-02-23 20:23:37 -0300

    Move files

--- server/src/Routes/products.ts
+++
@@ -1,9 +0,0 @@
-import { getproduc ,postproduct , borradologico ,getid} from "../Controlllers/products";
-import { Router} from "express";
-const router = Router();
-
-router.get("/", getproduc);
-router.post("/",postproduct)
-router.delete("/:id",borradologico)
-router.get("/:id",getid)
-export default router;
